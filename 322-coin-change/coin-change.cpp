dcf364d2730a9b2a3af7a27100e6f007e3a4ca01
AMAN SANGWAN <162349296+amansangwan1@users.noreply.github.com>
2026-05-12 01:04:26 +0530

    Time: 39 ms (42.02%) | Memory: 21.8 MB (45.58%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -22,7 +22,27 @@ public:
         
     }
 
+    int solveTab(vector<int>& coins,int amount){
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                int ans=INT_MAX;
+                if(i-coins[j]>=0){
+                    ans=dp[i-coins[j]];
+                }
+                if(ans!=INT_MAX){
+                    dp[i]=min(dp[i],1+ans);
+                }
+            }
+        }
+        return dp[amount];
+    }
+
     int coinChange(vector<int>& coins, int amount) {
+
+        return (solveTab(coins,amount)==INT_MAX)? -1:solveTab(coins,amount);
+        
         
         vector<int> dp(amount+1,-1);
         int ans=solve(coins,amount,dp);
